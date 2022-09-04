cf74d24ffb2485092b14f989cc07fc751e1ff545
kitajchuk <bk@kitajchuk.com>
2022-09-03 19:55:00 -0700

    Better scaffold 11ty

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,11 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.ignores.add("src");
   eleventyConfig.ignores.add("README.md");
   eleventyConfig.addPassthroughCopy({ "src/images": "images" });
   eleventyConfig.addPassthroughCopy({ "src/styles.css": "styles.css" });
+
+  return {
+    dir: {
+      input: "src",
+    },
+  };
 };

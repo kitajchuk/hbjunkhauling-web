3f914978a71d30e960d792d79b12d34eb9279add
kitajchuk <bk@kitajchuk.com>
2022-09-06 11:30:39 -0700

    Initial start on UI

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,18 @@
+const CleanCSS = require("clean-css");
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.ignores.add("README.md");
-  eleventyConfig.addPassthroughCopy({ "src/images": "images" });
-  eleventyConfig.addPassthroughCopy({ "src/styles.css": "styles.css" });
+
+  eleventyConfig.addFilter("cssmin", function(code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
+
+  eleventyConfig.addPassthroughCopy({ "src/public/favicon*": "." });
+  eleventyConfig.addPassthroughCopy({ "src/public/icon*.png": "." });
+  eleventyConfig.addPassthroughCopy({ "src/public/fonts": "fonts" });
+  eleventyConfig.addPassthroughCopy({ "src/public/images": "images" });
+  eleventyConfig.addPassthroughCopy({ "src/public/styles.css": "styles.css" });
+  eleventyConfig.addPassthroughCopy({ "src/public/manifest.json": "manifest.json" });
 
   return {
     dir: {

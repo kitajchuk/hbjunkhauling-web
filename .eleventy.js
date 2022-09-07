4ed8ab8522ab18c1b06e571035d79340474e210f
kitajchuk <bk@kitajchuk.com>
2022-09-07 14:22:20 -0700

    minify styles and html

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const CleanCSS = require("clean-css");
+const htmlmin = require("html-minifier");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.ignores.add("README.md");
@@ -7,11 +8,22 @@ module.exports = function(eleventyConfig) {
     return new CleanCSS({}).minify(code).styles;
   });
 
+  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+    if (outputPath && outputPath.endsWith(".html")) {
+      return htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      });
+    }
+
+    return content;
+  });
+
   eleventyConfig.addPassthroughCopy({ "src/public/favicon*": "." });
   eleventyConfig.addPassthroughCopy({ "src/public/icon*.png": "." });
   eleventyConfig.addPassthroughCopy({ "src/public/fonts": "fonts" });
   eleventyConfig.addPassthroughCopy({ "src/public/images": "images" });
-  eleventyConfig.addPassthroughCopy({ "src/public/styles.css": "styles.css" });
   eleventyConfig.addPassthroughCopy({ "src/public/manifest.json": "manifest.json" });
 
   return {

956f215864328a59263f47154ce1eceb354d0e4b
kitajchuk <bk@kitajchuk.com>
2023-05-08 19:18:18 -0700

    Add prettier, lint-staged and husky

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,14 @@
 const CleanCSS = require("clean-css");
 const htmlmin = require("html-minifier");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.ignores.add("README.md");
 
-  eleventyConfig.addFilter("cssmin", function(code) {
+  eleventyConfig.addFilter("cssmin", function (code) {
     return new CleanCSS({}).minify(code).styles;
   });
 
-  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
     if (outputPath && outputPath.endsWith(".html")) {
       return htmlmin.minify(content, {
         useShortDoctype: true,
@@ -23,7 +23,9 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/public/favicon*": "." });
   eleventyConfig.addPassthroughCopy({ "src/public/icon*.png": "." });
   eleventyConfig.addPassthroughCopy({ "src/public/images": "images" });
-  eleventyConfig.addPassthroughCopy({ "src/public/manifest.json": "manifest.json" });
+  eleventyConfig.addPassthroughCopy({
+    "src/public/manifest.json": "manifest.json",
+  });
 
   return {
     dir: {

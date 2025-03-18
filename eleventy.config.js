dec7f2c49b6ccabf5bf0253a7c61b7b733a20435
kitajchuk <bk@kitajchuk.com>
2025-03-18 08:29:18 -0700

    add vite plugin

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,11 +1,17 @@
 import "dotenv/config";
-import CleanCSS from "clean-css";
+import EleventyVitePlugin from "@11ty/eleventy-plugin-vite";
 
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ public: "." });
+  eleventyConfig.addPassthroughCopy({ "src/css": "css" });
 
-  eleventyConfig.addFilter("cssmin", function (code) {
-    return new CleanCSS({}).minify(code).styles;
+  eleventyConfig.addPlugin(EleventyVitePlugin, {
+    // Vite options can go here...
+  });
+
+  eleventyConfig.setLiquidOptions({
+    // https://www.11ty.dev/docs/languages/liquid/#java-script-truthiness-in-liquid
+    jsTruthy: true,
   });
 
   return {
